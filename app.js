ad5cd1b83268b6abfaad33f0708bd16e7d8df401
niel00 <danieldps911@hotmail.com>
2025-03-18 21:48:56 -0300

    Função sortear amigo e melhorias no código

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let listaAmigos = [];
 
-function adicionarAmigo(){
+function adicionarAmigo() {
     let nomeEscolhido = document.querySelector('input').value;
     if (nomeEscolhido == '') {
         alert('Por favor, insira um nome');
@@ -12,13 +12,28 @@ function adicionarAmigo(){
         atualizarListaDeAmigos();
     }
 }
+
 function atualizarListaDeAmigos() {
     let lista = document.getElementById('listaAmigos');
     lista.innerHTML = '';
-
+    let textoLista = document.getElementById('textoLista');
+    textoLista.innerHTML = 'Lista de participantes:';
     let nomes = '';
     for (let i = 0; i < listaAmigos.length; i++) {
         nomes += '<li>'+listaAmigos[i]+'</li>';
     }
     lista.innerHTML = nomes;
+}
+
+function sortearAmigo() {
+    let tamanho = listaAmigos.length;
+    let indiceAleatorio = Math.floor(Math.random() * (tamanho));
+    let textoResultado = document.getElementById('textoResultado');
+    if (listaAmigos.length === 0) {
+        alert('Adicione um amigo primeiro');
+    } else {
+        textoResultado.innerHTML = 'O seu amigo secreto é:';
+        resultado = document.getElementById('resultado')
+        resultado.innerHTML = listaAmigos[indiceAleatorio];
+    }
 }
